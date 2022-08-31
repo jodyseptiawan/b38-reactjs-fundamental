b5dbf2e95bc7d5b203367d25d547f1426c36627d
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-31 11:23:14 +0700

    Event

--- src/event.js
+++ src/event.js
@@ -2,10 +2,16 @@ import React from "react";
 
 function Event() {
   //Create function here
+  function halo() {
+    alert("Halo batch 38");
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <button onClick={halo}>click here</button>
+      <button onClick={() => alert(213)}>click here 2</button>
+    </div>
   );
 }
 
