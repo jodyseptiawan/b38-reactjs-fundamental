3123a64e01be515eb425bb26b0c6c4924837032d
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-31 11:05:40 +0700

    Embed

--- src/embedExpression.jsx
+++ src/embedExpression.jsx
@@ -2,12 +2,20 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function getMajor() {
+    return "Full stack";
+  }
 
   //Create a variable here
+  const name = "Dumbways";
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>
+        Welcome to {name} Class {getMajor()} {name}
+      </p>
+    </div>
   );
 }
 
